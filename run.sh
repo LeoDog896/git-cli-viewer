dcfc836c6a2c4a235ee1656738312db848db3273
LeoDog896 <LeoDog896@hotmail.com>
2022-01-20 18:20:38 +0000

    Fix up simple run script

--- run.sh
+++ run.sh
@@ -3,26 +3,25 @@
 # Create a new temp directory
 tmp_dir=$(mktemp -d -t git-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
 
-if [ "$#" == "1" ]
+if [ "$#" = "1" ]
 then
-	GithubDiscoveryURL = $1
+	githubDiscoveryURL=$1
 else
-	if [ "$1" == "github" ]
+	if [ "$1" = "github" ]
 	then
-		GithubDiscoveryURL = "https://github.com/$2"
-	elif [ "$1" == "gitlab" ]
+		githubDiscoveryURL="https://github.com/$2"
+	elif [ "$1" = "gitlab" ]
 	then
-		GithubDiscoveryURL = "https://gitlab.com/$2"
-	elif [ "$1" == "git" ]
+		githubDiscoveryURL="https://gitlab.com/$2"
+	elif [ "$1" = "git" ]
 	then
-		GithubDiscoveryURL = "$2"
+		githubDiscoveryURL="$2"
 	fi
 fi
 
-git clone $GithubDiscoveryURL $tmp_dir --depth=1
+echo $githubDiscoveryURL
 
-# Clone the repository
-git clone https://github.com/Project-Cepi/Sabre $tmp_dir --depth=1
+git clone $githubDiscoveryURL $tmp_dir --depth=1
 
 (cd $tmp_dir; eval $SHELL)
 
